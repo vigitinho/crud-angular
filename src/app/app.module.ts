803033f0540ef8f37e49fe45ea5d92d5ecc6a768
victor christoffoli <victorchristoffoli@outlook.com>
2022-09-11 17:25:38 -0300

    Primeiro Video Completo
    Primeiro Video Completo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,27 +6,44 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './views/home/home.component';
 import { HeaderComponent } from './shared/header/header.component';
 import { FooterComponent } from './shared/footer/footer.component';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatTableModule } from '@angular/material/table';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
+import { MatDialogModule } from '@angular/material/dialog';
+import { ElementDialogComponent } from './shared/element-dialog/element-dialog.component';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { FormsModule } from '@angular/forms';
+
+
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    ElementDialogComponent,
+    
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    NoopAnimationsModule,
     MatToolbarModule,
     MatTableModule,
     MatButtonModule, 
-    MatIconModule
+    MatIconModule,
+    MatDialogModule, 
+    MatFormFieldModule,
+    MatInputModule,
+    FormsModule
   ],
+ 
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  
 })
 export class AppModule { }
